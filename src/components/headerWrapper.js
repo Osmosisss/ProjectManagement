ad105fa118fb7d5e5940748987a5e362bc1926f9
Osmosisss <ozyosmosis@gmail.com>
2018-06-26 14:20:33 -0600

    setup off routes and headerWrapper components

---
+++ src/components/headerWrapper.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HeaderWrapper extends Component {
+  render() {
+    return (
+      <div className="header-wrapper">
+        <h1>Welcome to HOA Manager</h1>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default HeaderWrapper;
